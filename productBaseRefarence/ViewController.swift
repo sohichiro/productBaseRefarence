1bf15458c9adaa5b24e82ee01417ba8f6b258d44
長尾聡一郎 <urochiiuso_ogana_@i.softbank.jp>
2018-05-12 12:23:46 +0900

    Initial Commit

---
+++ productBaseRefarence/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  productBaseRefarence
+//
+//  Created by 長尾聡一郎 on 2018/05/12.
+//  Copyright © 2018年 Sohichiro Nagao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
